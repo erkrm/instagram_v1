8c12cecfa6c1addc555951c59a1bd8a0470207ce
unknown <erkroch@gmail.com>
2023-01-15 17:08:36 +0100

    create the header and image sections of the post component

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { DotsHorizontalIcon } from '@heroicons/react/solid';
 
-export default function Post({ id, username, userImg, img, caption }) {
+export default function Post({ username, userImg, img, caption, id }) {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="bg-white my-7  border rounded-md">
+      {/**POST HEADER */}
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold flex-1">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      {/**POST IMAGE */}
+
+      <img className="object-cover w-full" src={img} alt={username} />
     </div>
   );
 }
--- components/Posts.js
+++ components/Posts.js
@@ -6,7 +6,7 @@ export default function Posts() {
     {
       id: '1',
       username: 'admin',
-      userImageUrl:
+      userImg:
         'https://i.pinimg.com/originals/8e/9a/45/8e9a45396b98f9c31256ab7d589d6480.jpg',
       img: 'https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
       caption: 'great',
@@ -14,7 +14,7 @@ export default function Posts() {
     {
       id: '2',
       username: 'lolo',
-      userImageUrl:
+      userImg:
         'https://i.pinimg.com/originals/8e/9a/45/8e9a45396b98f9c31256ab7d589d6480.jpg',
       img: 'https://images.unsplash.com/photo-1671725501831-b7f678fa0241?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
       caption: 'beautiful',
@@ -22,7 +22,7 @@ export default function Posts() {
     {
       id: '3',
       username: 'sasa',
-      userImageUrl:
+      userImg:
         'https://i.pinimg.com/originals/8e/9a/45/8e9a45396b98f9c31256ab7d589d6480.jpg',
       img: 'https://images.unsplash.com/photo-1673261077665-581069aac65d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=906&q=80',
       caption: 'nice',
@@ -30,7 +30,7 @@ export default function Posts() {
     {
       id: '4',
       username: 'dede',
-      userImageUrl:
+      userImg:
         'https://i.pinimg.com/originals/8e/9a/45/8e9a45396b98f9c31256ab7d589d6480.jpg',
       img: 'https://images.unsplash.com/photo-1671725779305-bff966b40cbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
       caption: 'good',
@@ -38,7 +38,7 @@ export default function Posts() {
     {
       id: '5',
       username: 'coco',
-      userImageUrl:
+      userImg:
         'https://i.pinimg.com/originals/8e/9a/45/8e9a45396b98f9c31256ab7d589d6480.jpg',
       img: 'https://images.unsplash.com/photo-1673558347350-06f419b03e1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1033&q=80',
       caption: 'wonderful',
