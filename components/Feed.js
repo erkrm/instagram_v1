61094c7ae80a8e7820ba4448b2af029e12ecaf37
unknown <erkroch@gmail.com>
2023-01-15 10:28:02 +0100

    create posts and post components and some dummy data

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Stories from './Stories';
+import Posts from './Posts';
 
 export default function Feed() {
   return (
@@ -9,6 +10,7 @@ export default function Feed() {
         <Stories />
 
         {/**POSTS */}
+        <Posts />
       </section>
       <section>
         {/**MINI PROFILE */}
---
+++ components/Post.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Post({ id, username, userImg, img, caption }) {
+  return (
+    <div>
+      <h1>{username}</h1>
+    </div>
+  );
+}
