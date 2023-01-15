017444800585604b4454cdfb7ac43b4e65c3da83
unknown <erkroch@gmail.com>
2023-01-15 19:39:20 +0100

    fix de error in the feed section responsive

--- components/Feed.js
+++ components/Feed.js
@@ -5,7 +5,7 @@ import Posts from './Posts';
 export default function Feed() {
   return (
     <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
-      <section>
+      <section className="md:col-span-2">
         {/**STORIES */}
         <Stories />
 
