756e57ef6af02fefbeef7bde87d69a89cd39d6c8
unknown <erkroch@gmail.com>
2023-01-15 07:10:40 +0100

    populate fake using mnifaker

---
+++ components/Feed.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Stories from './Stories';
+
+export default function Feed() {
+  return (
+    <main>
+      <section>
+        {/**STORIES */}
+        <Stories />
+
+        {/**POSTS */}
+      </section>
+      <section>
+        {/**MINI PROFILE */}
+
+        {/**SUGGECTIONS */}
+      </section>
+    </main>
+  );
+}
