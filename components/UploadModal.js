1ba15363ecddce2ef2e2e7ebbf45b4c2d5f6bfe9
unknown <erkroch@gmail.com>
2023-01-17 15:10:40 +0100

    create a post and add it to firestore and get the download url

--- components/UploadModal.js
+++ components/UploadModal.js
@@ -3,11 +3,48 @@ import { useRecoilState } from 'recoil';
 import Modal from 'react-modal';
 import { CameraIcon } from '@heroicons/react/outline';
 import { useRef, useState } from 'react';
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from 'firebase/firestore';
+import { db, storage } from '@/firebase';
+import { useSession } from 'next-auth/react';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 
 export default function UploadModal() {
   const [open, setOpen] = useRecoilState(modalState);
-  const filePickerRef = useRef(null);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { data: session } = useSession();
+
+  async function uploadPost() {
+    if (loading) return;
+
+    setLoading(true);
+
+    const docRef = await addDoc(collection(db, 'posts'), {
+      caption: captionRef.current.value,
+      username: session.user.username,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+    await uploadString(imageRef, selectedFile, 'data_url').then(
+      async (snapshot) => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, 'posts', docRef.id), {
+          image: downloadURL,
+        });
+      }
+    );
+    setOpen(false);
+    setLoading(false);
+    setSelectedFile(null);
+  }
 
   function addImageToPost(event) {
     const reader = new FileReader();
@@ -18,6 +55,9 @@ export default function UploadModal() {
       setSelectedFile(readerEvent.target.result);
     };
   }
+  const filePickerRef = useRef(null);
+  const captionRef = useRef(null);
+
   return (
     <div>
       <h1>qqqqqq</h1>
@@ -57,10 +97,12 @@ export default function UploadModal() {
               maxLength="150"
               placeholder="Please enter your caption..."
               className="m-4 border-none text-center w-full focus:ring-0"
+              ref={captionRef}
             />
             <button
-              disabled
+              disabled={!selectedFile || loading}
               className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+              onClick={uploadPost}
             >
               Upload Post
             </button>
