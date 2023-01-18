d29c924ea14e4a12147340af6623bfd3d37f5288
unknown <erkroch@gmail.com>
2023-01-18 06:35:48 +0100

    add comment to the firebase database

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
+import React, { useState } from 'react';
 import {
   DotsHorizontalIcon,
   HeartIcon,
@@ -8,9 +8,24 @@ import {
   EmojiHappyIcon,
 } from '@heroicons/react/solid';
 import { useSession } from 'next-auth/react';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '@/firebase';
 
 export default function Post({ username, userImg, img, caption, id }) {
   const { data: session } = useSession();
+  const [comment, setComment] = useState('');
+
+  async function sendComment(event) {
+    event.preventDefault();
+    const commentToSend = comment;
+    setComment('');
+    await addDoc(collection(db, 'posts', id, 'comments'), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  }
   return (
     <div className="bg-white my-7  border rounded-md">
       {/**POST HEADER */}
@@ -51,11 +66,20 @@ export default function Post({ username, userImg, img, caption, id }) {
         <form className="flex items-center p-4">
           <EmojiHappyIcon className="h-7" />
           <input
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
             className="border-none flex-1 focus:ring-0"
             type="text"
             placeholder="Enter your comment..."
           />
-          <button className="text-blue-500 font-bold">Post</button>
+          <button
+            type="submit"
+            disabled={!comment.trim()}
+            className="text-blue-500 font-bold disabled:text-blue-200"
+            onClick={sendComment}
+          >
+            Post
+          </button>
         </form>
       )}
     </div>
